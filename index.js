62cb23dd4b5744c4b9fdfaf7797d1aadb08c7534
Fazle Rabbi Fahad <fazlerabbifahad.54@gmail.com>
2022-11-09 23:19:49 +0600

    added mongodb

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 //Add Dotenv
 require('dotenv').config();
 
@@ -12,6 +14,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//connect mongodb 
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.1bviphv.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
 
 
 
