2d8abfd07be764fe54f9caf0104e7cf1b4248fc2
Fazle Rabbi Fahad <fazlerabbifahad.54@gmail.com>
2023-02-15 14:08:49 +0600

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
@@ -15,25 +14,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-// function verifyJwt(req, res, next) {
-//     const authHeader = req.headers.authorization;
-//     console.log(authHeader);
-//     if (!authHeader) {
-//         res.status(401).send({ message: 'unauthorized access' })
-//     }
-
-//     const token = authHeader;
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-//         if (err) {
-//             res.status(401).send({ message: 'unauthorized access' })
-//         }
-//         req.decoded = decoded;
-//         next()
-//     })
-
-// }
-
 //connect mongodb 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1bviphv.mongodb.net/?retryWrites=true&w=majority`;
@@ -42,78 +22,65 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
-        const servicesCollection = client.db('photoGal').collection('services');
-        const reviewsCollection = client.db('photoGal').collection('reviews');
+        const productsCollection = client.db('cansoft').collection('products');
+        const usersCollection = client.db('cansoft').collection('users');
 
-        // app.post('/jwt', (req, res) => {
-        //     const user = req.body;
-        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10days' })
-        //     res.send({ token })
-        // })
 
-        app.get('/services', async (req, res) => {
+        app.get('/products', async (req, res) => {
             const query = {};
-            const cursor = servicesCollection.find(query);
-            const services = await cursor.toArray();
-            const servicesReverse = await services.reverse();
-            const limitedServices = await servicesReverse.slice(0, 3)
-            res.send({ servicesReverse, limitedServices })
+            const products = await productsCollection.find(query).toArray();
+            res.send(products)
         })
 
-        app.get('/services/:id', async (req, res) => {
+        app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const services = await servicesCollection.findOne(query);
-            res.send(services)
+            const products = await productsCollection.findOne(query);
+            res.send(products)
         })
 
-        app.post('/services', async (req, res) => {
-            const query = req.body;
-            const result = await servicesCollection.insertOne(query);
-            res.send(result);
-        })
-
-
-        //reviewsCollection
-        app.get('/reviews', async (req, res) => {
-            // const decoded = req.decoded;
-            // if (decoded.email !== req.query.email) {
-            //     res.status(403).send({ message: 'unauthorized access' })
-            // }
-
-            // let query = {};
-            // if (req.query.email) {
-            //     query = {
-            //         email: req.query.email
-            //     }
-            // }
-            const query = {};
-            const cursor = reviewsCollection.find(query);
-            const reviews = await cursor.toArray();
-            res.send(reviews);
-        })
-        app.get('/reviews/:id', async (req, res) => {
+        app.put('/products/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const cursor = reviewsCollection.filter(query => id === _id);
-            const results = await cursor.toArray();
+            const filter = { _id: ObjectId(id) };
+            const product = req.body;
+            const option = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    title: product.title,
+                    description: product.description,
+                    price: product.price,
+                    discountPercentage: product.discountPercentage,
+                    rating: product.rating,
+                    stock: product.stock,
+                    brand: product.brand,
+                    category: product.category
+                }
+            }
+            const results = await productsCollection.updateOne(filter, updateDoc, option);
             res.send(results)
         })
 
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users)
+        });
 
-        app.post('/reviews', async (req, res) => {
-            const query = req.body;
-            const result = await reviewsCollection.insertOne(query);
-            res.send(result)
-        })
-
-        app.delete('/reviews/:id', async (req, res) => {
+        app.get('/users/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const result = await reviewsCollection.deleteOne(query);
-            res.send(result);
+            const users = await usersCollection.findOne(query);
+            res.send(users)
+        });
 
+
+        app.post('/users', async (req, res) => {
+            const query = req.body;
+            const users = await usersCollection.insertOne(query);
+            res.send(users);
         })
+
+
 
 
 
@@ -130,9 +97,9 @@ run().catch(error => console.log(error))
 //server get and port
 
 app.get('/', (req, res) => {
-    res.send('PhotoGAL server is running');
+    res.send('CanSoft server is running');
 });
 
 app.listen(port, (req, res) => {
-    console.log(`The server is running on ${port}`);
+    console.log(`CanSoft server is running on ${port}`);
 })
